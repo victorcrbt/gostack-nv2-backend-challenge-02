b9988e7b4de25c60bdb6b1aa7f696707804afc09
Victor <madivcb@gmail.com>
2020-05-08 12:00:39 -0300

    create categories table migration

---
+++ src/database/migrations/1588949525613-create_categories_table.ts
@@ -0,0 +1,41 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateCategoriesTable1588949525613
+  implements MigrationInterface {
+  private tableName = 'categories';
+
+  private table = new Table({
+    name: this.tableName,
+    columns: [
+      {
+        name: 'id',
+        type: 'uuid',
+        isPrimary: true,
+        generationStrategy: 'uuid',
+        default: 'uuid_generate_v4()',
+      },
+      {
+        name: 'title',
+        type: 'varchar',
+      },
+      {
+        name: 'created_at',
+        type: 'timestamp',
+        default: 'now()',
+      },
+      {
+        name: 'updated_at',
+        type: 'timestamp',
+        default: 'now()',
+      },
+    ],
+  });
+
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.createTable(this.table);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropTable(this.tableName);
+  }
+}
